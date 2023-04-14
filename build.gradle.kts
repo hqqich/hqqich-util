ad0a14f7dee5074b6458ea6b7237b94ff20e67ca
hqqich <hqqich1314@outlook.com>
2023-04-14 15:08:50 +0800

    修改build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.20"
     application
+    id("java")
+    id("java-library")
+    id("maven-publish")
+    id("signing")
 }
 
 group = "com.github.hqqich"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
@@ -24,6 +29,6 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClass.set("MainKt")
-}
+//application {
+//    mainClass.set("MainKt")
+//}
