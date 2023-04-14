86a655f0704b96e56c738881a493cdeca2daf79b
hqqich <hqqich1314@outlook.com>
2023-04-14 16:09:12 +0800

    1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,28 @@ plugins {
 
 group = "com.github.hqqich"
 version = "1.0.0"
+description = "这是一个独立包"
+
+//sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
     mavenCentral()
     maven("https://jitpack.io")
 }
 
+buildscript{
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.0")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+    }
+}
+
 dependencies {
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.20")
     testImplementation(kotlin("test"))
 }
 
@@ -29,6 +44,24 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+afterEvaluate {
+    publishing {
+        publications {
+//            release(MavenPublication) {
+////                artifacts {  }
+////                artifactId = 'com.petterp.gradle.plugin'
+////                from components.java
+//            }
+//            // Creates a Maven publication called "release".
+//            release(MavenPublication) {
+//                from components.release
+//                        groupId = 'com.github.jitpack'
+//                artifactId = 'android-example'
+//                version = '1.0'
+//            }
+        }
+    }
+}
 //application {
 //    mainClass.set("MainKt")
 //}
