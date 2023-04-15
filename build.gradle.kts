9bab3d90b63150ba79d5deaaa1b5e9a50efc862d
hqqich <hqqich1314@outlook.com>
2023-04-15 08:13:50 +0800

    修改gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ publishing {
 
 group = "com.github.hqqich"
 version = "1.0.4"
-description = "这是一个独立包"
+description = "jar_by_JVM"
 
 //sourceCompatibility = JavaVersion.VERSION_1_8
 
@@ -53,4 +53,26 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+// 自定义任务
+task("myTask") {
+    println("Init myTask")
+    doFirst {
+        val result = cartesianProductString(arrayOf("a", "b", "c"), arrayOf("1", "2", "3"))
+        println(result)
+    }
+    doLast {
+        val result = cartesianProductString(arrayOf("1", "2", "3"), arrayOf("a", "b", "c"))
+        println(result)
+    }
+}
+
+// 函数
+fun cartesianProductString(array1: Array<String>, vararg arrays: Array<String>): List<String> {
+    return arrays.fold(array1.toList()) { acc, array ->
+        acc.flatMap { list ->
+            array.map { element -> list + element }
+        }
+    }
 }
